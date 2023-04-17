7289ecb44de8d4d68ed05d5715d010f550dc1818
Baddhet <tenjibiu@gmail.com>
2023-04-17 16:01:32 +0300

    feat: calc

---
+++ js/dropdown.js
@@ -0,0 +1,10 @@
+const dropdown = document.querySelector('.dropdown');
+const dropdownKitchen = document.querySelector('.dropdown__kitchen');
+const dropdownArrow = document.querySelector(".dropdown__arrow");
+const kitchen = document.querySelector('.kitchen');
+console.log(kitchen)
+dropdown.addEventListener('click', () => {
+  dropdownArrow.classList.toggle('expanded');
+  kitchen.classList.toggle('qwerty');
+  dropdownKitchen.classList.toggle('qwerty');
+})

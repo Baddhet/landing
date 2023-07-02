e37601ea2f40c135c16e90074998511296351d68
Baddhet <Tenjibiu@gmail.com>
2023-07-03 00:01:06 +0300

    fix: refactoring

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,11 +3,8 @@ const calcDropdown = document.querySelectorAll('.calc__dropdown');
 const dropdownArrow = document.querySelectorAll(".dropdown__arrow");
 
 for (let index = 0; index < dropdown.length; index++) {
-  const element = dropdown[index];
   dropdown[index].addEventListener('click', () => {
     dropdownArrow[index].classList.toggle('expanded');
     calcDropdown[index].classList.toggle('qwerty');
   });
 }
-
-
